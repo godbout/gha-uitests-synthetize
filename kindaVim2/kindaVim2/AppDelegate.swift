0762eda497d60583dd405ef24c9438aec0ee7dea
Guill Lo <guill.bout@gmail.com>
2021-05-24 01:40:59 +0800

    try new project from scratch with kv files

---
+++ kindaVim2/kindaVim2/AppDelegate.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    lazy var appComponent = AppComponent()
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        appComponent.setUp()
+    }
+
+}
+
+
